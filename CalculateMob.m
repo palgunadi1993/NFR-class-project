cb6813cb91c3d90e17ee3e099203aeab72f0e251
palgunadi1993 <palgunkh@kl-14723.kaust.edu.sa>
2019-11-24 13:17:10 +0300

    update fracture intersection

--- CalculateMob.m
+++ CalculateMob.m
@@ -1,5 +1,5 @@
 % ******************************************** Calculate  transmissibilities ****************************************************
-function  [Mob]=CalculateMob_noBC(matrix_frac_center,matrix_frac_nodes,max_intersection,Pij,Trans,num_matrix,matrix_nodes,new_fracture_nodes_pairs,new_fracture_id,Mob,Swij,swir,wexp,krwro,sorw,oexp,krocw,mu_o,mu_w)
+function  [Mob]=CalculateMob(matrix_frac_center,matrix_frac_nodes,max_intersection,Pij,Trans,num_matrix,matrix_nodes,new_fracture_nodes_pairs,new_fracture_id,Mob,Swij,swir,wexp,krwro,sorw,oexp,krocw,mu_o,mu_w)
 
 N = length(matrix_frac_center);
 Mob(1:N,1:4+max_intersection)=0;
@@ -27,10 +27,12 @@ for i=1:N
         else
             temp2 = nodes_pt(1,2);
         end
-        new_frac_neigh = frac_neigh(logical(sum(ismember(temp1,temp2),2)),:);
+        new_frac_neigh1 = frac_neigh(logical(sum(ismember(temp1,temp2),2)),:);
+        new_frac_neigh = [i;new_frac_neigh1];
+        neigh_ID(ismember(neigh_ID,new_frac_neigh)) = [];
     end
     
-    for j=1:num_neigh
+    for j=1:length(neigh_ID)
         p1=Pij(i); % pressure in the middle of the grid
         p2=Pij(neigh_ID(j));
         if(p2-p1>0)
@@ -44,8 +46,8 @@ for i=1:N
     end
     
     if num_neigh > 4
-        update_num = num_neigh + 1;
-        for z=1:length(new_frac_neigh)
+        update_num = length(neigh_ID) + 1;
+        for z=1:length(new_frac_neigh)-1
             c = z + 1;
             for d=c:length(new_frac_neigh)
                 p1=Pij(new_frac_neigh(z)); % pressure in the middle of the grid
